656e4755864183812fc6aa2440cc5db376a12682
Keenan Ng <keenandng@gmail.com>
2020-08-26 10:33:23 -0700

    word-reformer | Keenan Ng

---
+++ word-reformer/main.js
@@ -0,0 +1,6 @@
+function  wordReformer(wordFragment1, wordFragment2)  {
+  return wordFragment1 + wordFragment2
+}
+var wordReformerResult = wordReformer('java', 'script');
+console.log('WordReformer result:', wordReformerResult);
+console.log('Data type of wordReformerResult:', typeof wordReformerResult);
