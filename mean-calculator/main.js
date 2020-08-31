ce64e42c6c640ea9d534108310974eea3a1b82d9
Keenan Ng <keenandng@gmail.com>
2020-08-31 10:15:49 -0700

    mean-calculator | Keenan Ng

--- mean-calculator/main.js
+++ mean-calculator/main.js
@@ -1,6 +1,5 @@
-var total = 0;
-
 function meanCalculator(numbersArray) {
+  var total = 0;
   for (var i = 0; i < numbersArray.length; i++) {
   total += numbersArray[i];
   }
@@ -9,6 +8,6 @@ function meanCalculator(numbersArray) {
 
 // var numbersArray = [1, 2, 3, 4, 5];
 
-console.log(meanCalculator([1, 2, 3, 4, 5]))
-console.log(meanCalculator([7, 3, 1, 5, 3, 2]))
-console.log(meanCalculator([9, 3, 6]))
+console.log(meanCalculator([1, 2, 3, 4, 5]));
+console.log(meanCalculator([7, 3, 1, 5, 3, 2]));
+console.log(meanCalculator([9, 3, 6]));
