3be708e925d709ea0c328084340da16d066774bb
Keenan Ng <keenandng@gmail.com>
2020-08-31 10:35:19 -0700

    added toFixed(2) to limit it to 2 decimal places

--- mean-calculator/main.js
+++ mean-calculator/main.js
@@ -2,8 +2,9 @@ function meanCalculator(numbersArray) {
   var total = 0;
   for (var i = 0; i < numbersArray.length; i++) {
   total += numbersArray[i];
+  var mean = total / (numbersArray.length);
   }
-  return total / (numbersArray.length);
+  return mean.toFixed(2);
 }
 
 // var numbersArray = [1, 2, 3, 4, 5];
@@ -11,3 +12,4 @@ function meanCalculator(numbersArray) {
 console.log(meanCalculator([1, 2, 3, 4, 5]));
 console.log(meanCalculator([7, 3, 1, 5, 3, 2]));
 console.log(meanCalculator([9, 3, 6]));
+console.log(meanCalculator([25, 50, 25]));
