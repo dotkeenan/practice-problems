41eb59c53ef9e92e210fc81c0840e28ff8d63535
Keenan Ng <keenandng@gmail.com>
2020-09-09 10:37:47 -0700

    update

--- remove-null-values/main.js
+++ remove-null-values/main.js
@@ -16,3 +16,15 @@ function removeNullValues(array)  {
 console.log(removeNullValues([3, 5, null, null, 87, "hello", "goodbye"])) // -> [3,5,87,"hello", "goodbye"]
 console.log(removeNullValues([true, null, false, {}, null, 43, "node"])) // -> [true, false, {}, 43, "node"]
 console.log(removeNullValues([null, null, null, 7, null, "eight"])) // -> [7, "eight"]
+
+// my old solution that didnt work because I didn't know that there is no
+// ! = = =.  ! = = is actually 'not strictly equal'
+// function removeNullValues(array) {
+//   nonNulls = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] !== null) {
+//       nonNulls.push(array[i]);
+//     }
+//   }
+//   return nonNulls;
+// }
