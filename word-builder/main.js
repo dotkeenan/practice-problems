a50d886954845efe635b490354993d6702df5d4a
Keenan Ng <keenandng@gmail.com>
2020-09-10 10:15:44 -0700

    pp-word-builder | Keenan Ng

---
+++ word-builder/main.js
@@ -0,0 +1,16 @@
+function wordBuilder(wordToSpell) {
+  var word = '';
+  var wordArray = [];
+  if(typeof wordToSpell === 'string') {
+    for(i = 0; i < wordToSpell.length; i++) {
+      word += wordToSpell[i];
+      wordArray.push(word)
+      console.log('wordArray:', wordArray);
+    }
+  } else return
+}
+
+wordBuilder('hackathon');
+wordBuilder("JavaScript") // -> ["J", "Ja", "Jav", "Java", "JavaS", "JavaSc", "JavaScr", "JavaScri", "JavaScrip", "JavaScript"]
+wordBuilder("Moon") // -> ["M", "Mo", "Moo", "Moon"]
+wordBuilder("Stars") // -> ["S", "St", "Sta", "Star", "Stars"]
