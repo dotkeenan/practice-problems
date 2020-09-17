387d15045e08a0d21c3e8f249355aa67d3c6a9db
Keenan Ng <keenandng@gmail.com>
2020-09-16 20:28:04 -0700

    zip-code-validator | Keenan Ng

--- zip-code-validator/main.js
+++ zip-code-validator/main.js
@@ -1,29 +1,31 @@
-function zipCodeValidator(zipToValidate)  {
-  var parsed = '';
-  // var parsedArr = [];
-  for (var i = 0; i < zipToValidate.length; i++) {
-    parsed = parseInt(zipToValidate[i]);
-    console.log(typeof parsed, parsed)
-    // parsedArr.push(parsed);
-    if(typeof parsed !== 'number' || zipToValidate.length !== 5) {
-      return false;
-    }
-  }
-  return true;
+
+// This works because isNaN() type coerces strings to numbers.
+function zipCodeValidator(zipToValidate) {
+  return !isNaN(zipToValidate) && zipToValidate.length === 5
 }
 
-// console.log('21594 is a valid zip?',zipCodeValidator("21594")) // -> true
-// console.log('21594 is a valid zip?',zipCodeValidator("3Eds32")) // -> false
-// console.log('9123465 is a valid zip?',zipCodeValidator("9123465")) // -> false
-// console.log('55555 is a valid zip?',zipCodeValidator("55555")) // -> false
-console.log('ddddd is a valid zip?',zipCodeValidator("ddddd")) // -> false
+console.log('21594 is a valid zip?', zipCodeValidator("21594")) // -> true
+console.log('21594 is a valid zip?', zipCodeValidator("3Eds32")) // -> false
+console.log('9123465 is a valid zip?', zipCodeValidator("9123465")) // -> false
+console.log('55555 is a valid zip?', zipCodeValidator("55555")) // -> false
+console.log('ddddd is a valid zip?', zipCodeValidator("ddddd")) // -> false
 
-function zipCodeValidator(zipToValidate)  {
-  for (var i = 0; i < zipToValidate.length; i++)  {
-    parsed = parseInt(zipToValidate[i]);
+// Old way.  Doesn't work on last example.
+// function zipCodeValidator(zipToValidate) {
+//   var parsed = '';
+//   // var parsedArr = [];
+//   for (var i = 0; i < zipToValidate.length; i++) {
+//     parsed = parseInt(zipToValidate[i]);
+//     // parsedArr.push(parsed);
+//     if (typeof parsed !== 'number' || zipToValidate.length !== 5) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-  }
-}
-
-// cody solution
-// return !isNaN(zipToValidate) && zipToValidate.length === 5
+// console.log('21594 is a valid zip?', zipCodeValidator("21594")) // -> true
+// console.log('3Eds32 is a valid zip?', zipCodeValidator("3Eds32")) // -> false
+// console.log('9123465 is a valid zip?', zipCodeValidator("9123465")) // -> false
+// console.log('55555 is a valid zip?', zipCodeValidator("55555")) // -> false
+// console.log('ddddd is a valid zip?', zipCodeValidator("ddddd")) // -> false
