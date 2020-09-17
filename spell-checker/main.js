5848327d706f8c1ec148ad63f37d4899c894bcb2
Keenan Ng <keenandng@gmail.com>
2020-09-16 18:24:15 -0700

    pp-spell-checker | Keenan Ng

--- spell-checker/main.js
+++ spell-checker/main.js
@@ -20,21 +20,16 @@ Goal: create a function that takes two arrays as arguments.  Return an object
 with two properties: two arrays in which one array contains misspelled words,
  and the other array contains correctly spelled words.
 
+define a function named spellChecker that takes two parameters: userWords, correctWords
 create an empty object named spellingObject
-
-//not needed
-create an empty array named correct
 create an empty array named incorrect
 
-define a function named spellChecker that takes two parameters: userWords, correctWords
-
 use a for loop to check each if the value at each index of userWords is === to
-the value of the same index of correctWords
+the value at the same index of correctWords
+
+if it doesn't pass, add userWords[i] to the array named incorrect.
 
 Stop the loop when there are no more items in the userWords array.
-
-if the check passes, add userWords[i] to the array named correct.
-if it doesn't pass, add userWords[i] to the array named incorrect.
 
 
 add the array named incorrect and function parameter named correctWords
