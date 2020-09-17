3a82fa2adb7c495bd42ec041a09f9361cd8c89f1
Keenan Ng <keenandng@gmail.com>
2020-09-16 20:28:39 -0700

    zip-code-validator | Keenan Ng

--- zip-code-validator/main.js
+++ zip-code-validator/main.js
@@ -5,7 +5,7 @@ function zipCodeValidator(zipToValidate) {
 }
 
 console.log('21594 is a valid zip?', zipCodeValidator("21594")) // -> true
-console.log('21594 is a valid zip?', zipCodeValidator("3Eds32")) // -> false
+console.log('3Eds32 is a valid zip?', zipCodeValidator("3Eds32")) // -> false
 console.log('9123465 is a valid zip?', zipCodeValidator("9123465")) // -> false
 console.log('55555 is a valid zip?', zipCodeValidator("55555")) // -> false
 console.log('ddddd is a valid zip?', zipCodeValidator("ddddd")) // -> false
