0ac5ba58353f1dfdc71cba8f7abbe1e2db3d1e12
Keenan Ng <keenandng@gmail.com>
2020-09-21 10:52:26 -0700

    pp-finding-nemo | Non-method way included | Keenan Ng

--- finding-nemo/main.js
+++ finding-nemo/main.js
@@ -15,9 +15,9 @@ split up the sentence into individual words and save that into a storage named s
 
 create a way to check each item in the storage
 
-check to see if it is equal to the string 'Nemo'
+check to see if it is equal to the word 'Nemo'
 
-if it is, return the current index position it finds it at offset by a value of + 1
+if it is, return the current index position it finds it at, offset by a value of + 1
 due to array index's starting at 0.
 
 */
@@ -29,8 +29,34 @@ function findingNemo(sentence)  {
       return i + 1;
     }
   }
+  return 'Nemo not found in sentence';
 }
 
-console.log('Position of "Nemo" in "I am Nemo"', findingNemo("I am Nemo")) // -> 3
-console.log('Position of "Nemo" in "Where is Nemo my son?"',findingNemo("Where is Nemo my son?")) // -> 3
-console.log('Position of "Nemo" in "Nemo is my name"',findingNemo("Nemo is my name")) // -> 1
+console.log('Position of "Nemo" in "I am Nemo":', findingNemo("I am Nemo")) // -> 3
+console.log('Position of "Nemo" in "Where is Nemo my son?":',findingNemo("Where is Nemo my son?")) // -> 3
+console.log('Position of "Nemo" in "Nemo is my name":',findingNemo("Nemo is my name")) // -> 1
+
+/*  Non method way
+function findingNemo(sentence)  {
+  var word = '';
+  var result = [];
+  for (var i = 0; i < sentence.length; i++) {
+    if  (sentence[i] === ' ' || sentence[i] === undefined)  {
+      result.push(word);
+      word = '';
+    } else  {
+      word += sentence[i];
+    }
+  }
+
+  for (var x = 0; x < result.length; x++) {
+    if(result[x] === 'Nemo')  {
+      return x + 1;
+    }
+  }
+  return "No Nemo found";
+}
+console.log('Position of "Nemo" in "I am Nemo":', findingNemo("I am Nemo")) // -> 3
+console.log('Position of "Nemo" in "Where is Nemo my son?":',findingNemo("Where is Nemo my son?")) // -> 3
+console.log('Position of "Nemo" in "Nemo is my name":',findingNemo("Nemo is my name")) // -> 1
+*/
